71e4bc92bf7eb8d094e2602f610823d050736695
netcell <anhnt.fami@gmail.com>
2019-04-08 19:04:16 +0700

    Replace express-session by cookie-session

--- src/app.mjs
+++ src/app.mjs
@@ -2,25 +2,24 @@ import express from 'express';
 import passport from 'passport';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import session from 'express-session';
+import session from 'cookie-session';
 import User from './Models/User';
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 export const app = express();
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
 	secret: process.env.SESSION_SECRET,
 	saveUninitialized: true,
 	resave: true
 }));
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
 app.use(passport.initialize());
 app.use(passport.session());
